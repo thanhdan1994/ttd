d218af90381ec2d58db840cd507735fa409c692b
TRAN THANH DAN <thanhdan26081994@gmail.com>
2020-11-01 20:44:43 +0700

    update config pusher

--- ttd/resources/js/bootstrap.js
+++ ttd/resources/js/bootstrap.js
@@ -38,7 +38,7 @@ window.Pusher = require('pusher-js');
 
 window.Echo = new Echo({
     broadcaster: 'pusher',
-    key: '84a7b4c63127f7636646',
-    cluster: 'ap1',
+    key: process.env.MIX_PUSHER_APP_KEY,
+    cluster: process.env.MIX_PUSHER_APP_CLUSTER,
     forceTLS: true
 });
